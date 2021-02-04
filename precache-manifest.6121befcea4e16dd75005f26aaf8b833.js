0b09d3b771fa3a0c44fc26ce5fa122c307ceb64a
Zheronimo <kirichenko.e.o@gmail.com>
2021-02-04 20:50:41 +0200

    Updates

---
+++ precache-manifest.6121befcea4e16dd75005f26aaf8b833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee61241be5a2d9d3e54689adb82c859f",
+    "url": "/To-do-React-/index.html"
+  },
+  {
+    "revision": "130d092f26fc93bc1631",
+    "url": "/To-do-React-/static/css/main.b640cefb.chunk.css"
+  },
+  {
+    "revision": "53bf6351f5b5ee97a9bd",
+    "url": "/To-do-React-/static/js/2.8ba249f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-React-/static/js/2.8ba249f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130d092f26fc93bc1631",
+    "url": "/To-do-React-/static/js/main.1df0c14b.chunk.js"
+  },
+  {
+    "revision": "49538302dbaff26a27d9",
+    "url": "/To-do-React-/static/js/runtime-main.3af10229.js"
+  }
+]);
